d1d97ecaed044f0c790179b6fee6f56b78336149
Milos Nikic <nikic.milos@gmail.com>
2025-07-12 23:04:38 +0100

    mkdir_p should work now too

--- libdiskfs/write.c
+++ libdiskfs/write.c
@@ -7,93 +7,6 @@
 #include <unistd.h>
 
 #define MAX_PATH_LEN  1024
-static void
-make_dir_safe (struct node *root, const char *dirname)
-{
-  error_t err;
-  struct node *new_node = NULL;
-  struct protid *cred = NULL;
-  struct peropen *po = NULL;
-  struct dirstat *ds = alloca (diskfs_dirstat_size);
-
-  fprintf (stderr, "[DEBUG] make_dir: start\n");
-
-  err = diskfs_make_peropen (root, O_READ | O_EXEC | O_WRITE, 0, &po);
-  if (err)
-    {
-      fprintf (stderr, "[ERROR] make_peropen failed: %d\n", err);
-      return;
-    }
-
-  err = diskfs_create_protid (po, 0, &cred);
-  if (err)
-    {
-      fprintf (stderr, "[ERROR] create_protid failed: %d\n", err);
-      ports_port_deref (po);
-      return;
-    }
-
-  pthread_mutex_lock (&root->lock);
-
-  // STEP 1: Try LOOKUP to see if it already exists
-  err = diskfs_lookup (root, dirname, LOOKUP, &new_node, NULL, cred);
-  if (err == 0 && new_node != NULL)
-    {
-      fprintf (stderr, "[DEBUG] Directory already exists\n");
-      pthread_mutex_unlock (&new_node->lock);
-      fprintf (stderr, "[DEBUG] Unlocked new node %llu\n",
-	       new_node->dn_stat.st_ino);
-      diskfs_nput (new_node);
-      fprintf (stderr, "[DEBUG] After nput %llu\n", new_node->dn_stat.st_ino);
-      pthread_mutex_unlock (&root->lock);
-      fprintf (stderr, "[DEBUG] After unlick root %llu\n",
-	       root->dn_stat.st_ino);
-      goto cleanup;
-    }
-  else if (err != ENOENT)
-    {
-      fprintf (stderr, "[ERROR] lookup(LOOKUP) failed: %d\n", err);
-      pthread_mutex_unlock (&root->lock);
-      goto cleanup;
-    }
-
-  // STEP 2: Use CREATE with dirstat to set up for create_node
-  err = diskfs_lookup (root, dirname, CREATE, NULL, ds, cred);
-  if (err)
-    {
-      fprintf (stderr, "[ERROR] lookup(CREATE) failed: %d\n", err);
-      pthread_mutex_unlock (&root->lock);
-      diskfs_drop_dirstat (root, ds);
-      goto cleanup;
-    }
-
-  // STEP 3: Actually create the node
-  mode_t mode = S_IFDIR | 0755;
-  err = diskfs_create_node (root, dirname, mode, &new_node, cred, ds);
-  if (err)
-    {
-      fprintf (stderr, "[ERROR] create_node failed: %d\n", err);
-      pthread_mutex_unlock (&root->lock);
-      diskfs_drop_dirstat (root, ds);
-      goto cleanup;
-    }
-
-  fprintf (stderr, "[DEBUG] Directory '%s' created\n", dirname);
-  diskfs_node_update (new_node, 1);
-  pthread_mutex_unlock (&new_node->lock);
-  diskfs_nput (new_node);
-
-  pthread_mutex_unlock (&root->lock);
-  diskfs_drop_dirstat (root, ds);
-
-cleanup:
-  if (cred)
-    {
-      ports_port_deref (cred);
-      fprintf (stderr, "[DEBUG] cleaned up cred.\n");
-    }
-  fprintf (stderr, "[DEBUG] make_dir: success\n");
-}
 
 static void
 make_dir (struct node *root, const char *dirname)
@@ -165,6 +78,28 @@ cleanup:
 static struct node *
 mkdir_p (struct node *root, const char *path)
 {
+
+  error_t err;
+  struct protid *cred = NULL;
+  struct peropen *po = NULL;
+
+  fprintf (stderr, "[DEBUG] makedir_p: start\n");
+
+  err = diskfs_make_peropen (root, O_READ | O_EXEC | O_WRITE, 0, &po);
+  if (err)
+    {
+      fprintf (stderr, "[ERROR] make_peropen failed: %d\n", err);
+      return NULL;
+    }
+
+  err = diskfs_create_protid (po, 0, &cred);
+  if (err)
+    {
+      fprintf (stderr, "[ERROR] create_protid failed: %d\n", err);
+      ports_port_deref (po);
+      return NULL;
+    }
+
   char path_copy[MAX_PATH_LEN];
   strncpy (path_copy, path, MAX_PATH_LEN);
 
@@ -174,11 +109,13 @@ mkdir_p (struct node *root, const char *path)
   struct node *prev_node = NULL;
   while (token != NULL)
     {
+
+      fprintf (stderr, "[DEBUG] Token: %s\n", token);
       make_dir (root, token);
 
       struct node *next_node = NULL;
-      error_t err =
-	diskfs_lookup (root, token, LOOKUP, &next_node, NULL, NULL);
+      error_t err = diskfs_lookup (root, token, LOOKUP, &next_node, 0, cred);
+      fprintf (stderr, "[DEBUG] After lookup for token  %s\n", token);
       if (err || !next_node)
 	{
 	  fprintf (stderr,
@@ -186,9 +123,14 @@ mkdir_p (struct node *root, const char *path)
 		   token);
 	  if (prev_node)
 	    diskfs_nput (prev_node);
+
+	  ports_port_deref (cred);
 	  return NULL;
 	}
 
+      fprintf (stderr, "[DEBUG] Found a node. Gonna try unlock.  %s\n", token);
+      pthread_mutex_unlock (&next_node->lock);
+      fprintf (stderr, "[DEBUG] Unlocked it.  %s\n", token);
       if (prev_node)
 	diskfs_nput (prev_node);
       prev_node = root;
@@ -197,6 +139,7 @@ mkdir_p (struct node *root, const char *path)
       token = strtok (NULL, "/");
     }
 
+  ports_port_deref (cred);
   return root;
 }
 
@@ -208,12 +151,8 @@ test (void)
   struct node *root = diskfs_root_node;
   diskfs_nref (root);
 
-  make_dir (root, "hey");
-  make_dir (root, "hey");
-  make_dir (root, "hey2");
-  make_dir (root, "hey2");
-  make_dir (root, "hey3");
-  make_dir (root, "hey");
+  mkdir_p (root, "hey/there/you/too");
+
   diskfs_nput (root);
 }
 
