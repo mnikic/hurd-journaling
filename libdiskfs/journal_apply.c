9a5938194ec97864945e273a84349e1f30c14a20
Milos Nikic <nikic.milos@gmail.com>
2025-08-08 01:19:41 +0100

    Fixed the file creation during restore again by reverting a giant refucktoring

--- libdiskfs/journal_apply.c
+++ libdiskfs/journal_apply.c
@@ -21,13 +21,12 @@
 
 #include <libdiskfs/journal_apply.h>
 #include <libdiskfs/journal_config.h>
-#include <libdiskfs/journal_path_util.h>
 #include <libdiskfs/journal_util.h>
+#include <libdiskfs/journal_path_util.h>
 #include <libdiskfs/journal_format.h>
 #include <libdiskfs/journal_diskfs_helper.h>
 #include <libdiskfs/journal_policy.h>
 #include <libdiskfs/diskfs.h>
-
 #include <inttypes.h>
 #include <stdio.h>
 #include <string.h>
@@ -45,7 +44,7 @@
 
 static inline bool
 node_matches_fingerprint (const struct node *np,
-			  const inode_replay_state_t *state)
+			  const inode_replay_state_t * state)
 {
   const struct stat *st = &np->dn_stat;
   if (st->st_size != state->st_size || st->st_blocks != state->st_blocks
@@ -67,70 +66,9 @@ node_matches_fingerprint (const struct node *np,
   return true;
 }
 
-/* Common lookup logic used by file/dir create paths. Validates match if found. */
 static error_t
-lookup_node_and_check_fingerprint (struct node *fs_root,
-				   const char *path,
-				   const inode_replay_state_t *state,
-				   struct protid *cred, struct node **out)
-{
-  struct node *np = NULL;
-  error_t err = diskfs_lookup_path (fs_root, path, cred, &np);
-  if (!err && np)
-    {
-      if (!node_matches_fingerprint (np, state))
-	{
-	  JOURNAL_LOG_DEBUG ("Node %" PRIu64
-			     " found for path %s but doesn't match fingerprint. Skipping.",
-			     np->dn_stat.st_ino, path);
-	  diskfs_nput (np);
-	  *out = NULL;
-	  return EINVAL;
-	}
-      *out = np;
-      return 0;
-    }
-
-  if (err != ENOENT)
-    {
-      *out = NULL;
-      return err;
-    }
-
-  return ENOENT;		// Explicitly signal not found
-}
-
-/* Creates directory path (mkdir -p style). */
-static error_t
-create_directory (struct node *restore_root, const char *dir_path,
-		  struct protid *cred, struct node **out)
-{
-  error_t err = diskfs_mkdir_p (restore_root, dir_path, cred);
-  if (err)
-    {
-      JOURNAL_LOG_ERROR ("Failed to create directory '%s': %s",
-			 dir_path, strerror (err));
-      *out = NULL;
-      return err;
-    }
-
-  err = diskfs_lookup_path (restore_root, dir_path, cred, out);
-  if (err || !*out)
-    {
-      JOURNAL_LOG_ERROR ("Failed to re-lookup created directory '%s': %s",
-			 dir_path, strerror (err));
-      *out = NULL;
-      return err ? err : EIO;
-    }
-
-  return 0;
-}
-
-/* Validates and locates (or creates) a directory node. */
-static error_t
-find_or_create_directory (struct node *fs_root, struct node *restore_root,
-			  const char *dir_path,
-			  const inode_replay_state_t *state,
+find_or_create_directory (struct node *fs_root, struct node *restore_root, const char *dir_path,
+			  const inode_replay_state_t * state,
 			  struct protid *cred, struct node **out)
 {
   if (!journal_good_dir_path (dir_path))
@@ -141,21 +79,65 @@ find_or_create_directory (struct node *fs_root, struct node *restore_root,
       return EINVAL;
     }
 
-  error_t err =
-    lookup_node_and_check_fingerprint (fs_root, dir_path, state, cred, out);
-  if (err == 0 || err != ENOENT)
+  struct node *np = NULL;
+  error_t err = diskfs_lookup_path (fs_root, dir_path, cred, &np);
+  if (!err)
+    {
+      if (!node_matches_fingerprint (np, state))
+	{
+	  JOURNAL_LOG_DEBUG
+	    ("Node %" PRIu64
+	     " found for path %s but it doesn't match the fingerprint. Skipping.",
+	     np->dn_stat.st_ino, dir_path);
+	  diskfs_nput (np);
+	  return EINVAL;
+	}
+      JOURNAL_LOG_DEBUG
+	("ino %u: Directory found node. ino: %" PRIu64 ". Path: %s",
+	 state->ino, np->dn_stat.st_ino, dir_path);
+      *out = np;
+      return 0;
+    }
+
+  if (err != ENOENT)
     return err;
 
-  JOURNAL_LOG_DEBUG ("Inode %u: directory not found. Creating: %s",
-		     state->ino, dir_path);
-  return create_directory (restore_root, dir_path, cred, out);
+  JOURNAL_LOG_DEBUG
+    ("inode %u: Directory not found. Creating a new one. Path: %s",
+     state->ino, dir_path);
+
+  err = diskfs_mkdir_p (restore_root, dir_path, cred);
+  if (err)
+    {
+      JOURNAL_LOG_ERROR
+	("inode %u: Failed to create a directory. Skipping. Path: %s. Error: %s",
+	 state->ino, dir_path, strerror (err));
+      *out = NULL;
+      return err;
+    }
+
+  struct node *dir = NULL;
+  err = diskfs_lookup_path (restore_root, dir_path, cred, &dir);
+  if (err || !dir)
+    {
+      JOURNAL_LOG_DEBUG
+	("inode %u: It seems we failed to lookup dir mkdir_p create. Err: %s.",
+	 state->ino, strerror (err));
+      *out = NULL;
+      return err;
+    }
+
+  JOURNAL_LOG_DEBUG
+    ("inode %u: Directory created. Path: %s. New Ino: %" PRIu64 "",
+     state->ino, dir_path, dir->dn_stat.st_ino);
+  *out = dir;
+  return 0;
 }
 
-/* Validates and creates a file at the given path, if it does not already exist. */
 static error_t
 find_or_create_file (struct node *fs_root, struct node *restore_root,
 		     const char *full_path,
-		     const inode_replay_state_t *state, struct protid *cred,
+		     const inode_replay_state_t * state, struct protid *cred,
 		     struct node **out)
 {
   char dir_path[JOURNAL_PATH_MAX];
@@ -170,37 +152,71 @@ find_or_create_file (struct node *fs_root, struct node *restore_root,
 
   if (!journal_good_dir_path (dir_path))
     {
-      JOURNAL_LOG_DEBUG ("Rejected: ino %u directory path='%s' is invalid.",
-			 state->ino, dir_path);
+      JOURNAL_LOG_DEBUG
+	("Rejected: ino %u its directory path='%s' is not supported.",
+	 state->ino, dir_path);
       *out = NULL;
       return EINVAL;
     }
 
   if (!journal_good_filename (file_name))
     {
-      JOURNAL_LOG_DEBUG ("Rejected: ino %u filename '%s' is invalid.",
-			 state->ino, file_name);
+      JOURNAL_LOG_DEBUG
+	("Rejected: ino %u its file name '%s' is not supported.", state->ino,
+	 file_name);
       *out = NULL;
       return EINVAL;
     }
 
-  error_t err =
-    lookup_node_and_check_fingerprint (fs_root, full_path, state, cred, out);
-  if (err == 0 || err != ENOENT)
-    return err;
+  struct node *np = NULL;
+  error_t err = diskfs_lookup_path (fs_root, full_path, cred, &np);
+  if (!err)
+    {
+      if (!node_matches_fingerprint (np, state))
+	{
+	  JOURNAL_LOG_DEBUG
+	    ("Node %" PRIu64
+	     " found for path %s but it doesn't match the fingerprint. Skipping.",
+	     np->dn_stat.st_ino, full_path);
+	  diskfs_nput (np);
+	  *out = NULL;
+	  return EINVAL;
+	}
+      *out = np;
+      return 0;
+    }
 
-  JOURNAL_LOG_DEBUG
-    ("Inode %u: file not found. Creating: %s (dir: %s, name: %s)", state->ino,
-     full_path, dir_path, file_name);
-
-  struct node *dir = NULL;
-  err = create_directory (restore_root, dir_path, cred, &dir);
-  if (err || !dir)
+  if (err != ENOENT)
     {
       *out = NULL;
       return err;
     }
 
+  JOURNAL_LOG_DEBUG
+    ("inode %u: File not found. Creating new one. Path: %s. Dir: '%s' File: '%s'",
+     state->ino, full_path, dir_path, file_name);
+  err = diskfs_mkdir_p (restore_root, dir_path, cred);
+  if (err)
+    {
+      JOURNAL_LOG_ERROR
+	("inode %u: Failed to create a directory. Skipping. Path: %s. Error: %s",
+	 state->ino, dir_path, strerror (err));
+      *out = NULL;
+      return err;
+    }
+  struct node * dir = NULL;
+  JOURNAL_LOG_DEBUG
+    ("inode %u: In lookup path for the dir the came out of mkdir_p. Let see Dir: '%s'.",
+     state->ino, dir_path);
+  err = diskfs_lookup_path (restore_root, dir_path, cred, &dir);
+  if (err || !dir)
+    {
+      JOURNAL_LOG_DEBUG
+	("inode %u: It seems we failed to lookup dir mkdir_p create. Err: %s.",
+	 state->ino, strerror (err));
+      *out = NULL;
+      return err;
+    }
   struct node *file = NULL;
   err = diskfs_make_file (dir, file_name, cred, &file);
   if (diskfs_synchronous)
@@ -209,53 +225,98 @@ find_or_create_file (struct node *fs_root, struct node *restore_root,
       diskfs_file_update (dir, 1);
     }
   diskfs_nput (dir);
-
   if (err)
     {
-      JOURNAL_LOG_ERROR ("Inode %u: failed to create file '%s': %s",
-			 state->ino, full_path, strerror (err));
+      JOURNAL_LOG_ERROR
+	("inode %u: Failed to create a file. Skipping. Path: %s. Error: %s",
+	 state->ino, dir_path, strerror (err));
       *out = NULL;
       return err;
     }
 
-  JOURNAL_LOG_DEBUG ("Inode %u: file created at %s. New ino: %" PRIu64,
-		     state->ino, full_path, file->dn_stat.st_ino);
+  JOURNAL_LOG_DEBUG
+    ("inode %u: File created. Path: %s. New Ino: %" PRIu64 "",
+     state->ino, full_path, file->dn_stat.st_ino);
   *out = file;
   return 0;
 }
 
-/* Determines whether the inode is a dir or file and applies appropriate creation logic. */
-error_t
-find_by_path_or_create (const char *full_path,
-			const inode_replay_state_t *state,
-			struct node *fs_root, struct node *restore_root,
-			struct protid *cred, struct node **out)
-{
-  if (S_ISDIR (state->st_mode))
-    return find_or_create_directory (fs_root, restore_root, full_path, state,
-				     cred, out);
+static error_t
+find_by_path_or_create (inode_replay_state_t * state, struct node * fs_root,
+			struct node *restore_root, struct protid *cred,
+			struct node **out)
+{ char full_path[JOURNAL_NORMALIZED_PATH_MAX];
+  error_t err = journal_resolve_full_path (state->resolved_path, state->name,
+					   full_path, sizeof (full_path));
+  if (err)
+    {
+      *out = NULL;
+      return err;
+    }
+  uint32_t mode = state->st_mode;
+  if (S_ISDIR (mode))
+    return find_or_create_directory (fs_root, restore_root, full_path,
+				     state, cred, out);
   else
-    return find_or_create_file (fs_root, restore_root, full_path, state, cred,
-				out);
+    return find_or_create_file (fs_root, restore_root, full_path, state, cred, out);
 }
 
-static bool
-should_skip_inode (const inode_replay_state_t *state, struct node *np)
-{
-  if (!journal_is_safe_stat (np->dn_stat.st_mode))
-    return true;
-  if ((int64_t) np->dn_stat.st_mtime < 0
-      || (int64_t) np->dn_stat.st_ctime < 0)
-    return true;
-  if ((uint64_t) np->dn_stat.st_ctime >= state->last_seen)
-    return true;
-  return false;
-}
+error_t
+apply_node_replay (inode_replay_state_t * state,struct node *fs_root, struct node *restore_root,
+		   struct protid *cred) {
+  if (state->ino < JOURNAL_REPLAY_MIN_INO)
+    {
+      JOURNAL_LOG_DEBUG ("inode %" PRIu32
+			 " below REPLAY_MIN_INO (%d) skipping as potentially system-critical",
+			 state->ino, JOURNAL_REPLAY_MIN_INO);
+      return 0;
+    }
+  struct node *np = NULL;
+  char *path = state->resolved_path;
+  error_t err = diskfs_cached_lookup ((ino_t) state->ino, &np);
+  if (!err && np->dn_stat.st_mode > 0 && np->dn_stat.st_nlink > 0
+      && node_matches_fingerprint (np, state))
+    {
+      JOURNAL_LOG_DEBUG ("inode %" PRIu32
+			 " found! mode is %o and links %u that matches the fingerprint.",
+			 state->ino, np->dn_stat.st_mode,
+			 np->dn_stat.st_nlink);
+      if (!journal_is_safe_stat (np->dn_stat.st_mode))
+	{
+	  diskfs_nput (np);
+	  return 0;
+	}
 
-static int
-apply_metadata_changes (struct node *np, const inode_replay_state_t *state,
-			const char *path)
-{
+      if ((int64_t) np->dn_stat.st_mtime < 0
+	  || (int64_t) np->dn_stat.st_ctime < 0)
+	{
+	  diskfs_nput (np);
+	  return 0;
+	}
+
+      if ((uint64_t) np->dn_stat.st_ctime >= state->last_seen)
+	{
+	  diskfs_nput (np);
+	  return 0;
+	}
+    }
+  else
+    {
+      // First cleanup!
+      if (np)
+	{
+	  diskfs_nput (np);
+	  np = NULL;
+	}
+      JOURNAL_LOG_DEBUG
+	("It doesn't seem file ino: %u is there. Lets see if we can find it by name.",
+	 state->ino);
+      // Then action
+      err = find_by_path_or_create (state, fs_root, restore_root, cred, &np);
+      // All has failed
+      if (!np)
+	return err;
+    }
   int changes = 0;
   char change_desc[128];
   change_desc[0] = '\0';
@@ -269,17 +330,11 @@ apply_metadata_changes (struct node *np, const inode_replay_state_t *state,
       changes++;
     }
 
-  if (np->dn_stat.st_gid != state->gid)
+  if (
+       np->dn_stat.st_gid != state->gid)
     {
       APPEND_CHANGE ("gid");
       np->dn_stat.st_gid = state->gid;
-      changes++;
-    }
-
-  if (np->dn_stat.st_author != state->author)
-    {
-      APPEND_CHANGE ("author");
-      np->dn_stat.st_author = state->author;
       changes++;
     }
 
@@ -301,6 +356,12 @@ apply_metadata_changes (struct node *np, const inode_replay_state_t *state,
   if (state->has_ctime && np->dn_stat.st_ctime < state->ctime)
     {
       APPEND_CHANGE ("ctime");
+      // setting explicitly st_ctime value will be ignored by diskfs.
+      // dn_set_ctime flag is instead used without which any update 
+      // becomes (silently) ignored. 
+      // So we are forced to set dn_set_ctime to get anything done!
+      // st_ctime will be set to current (and no other) time by the diskfs if
+      // dn_set_ctime is set to 1. We will set it a bit down for any change.
       changes++;
     }
 
@@ -321,67 +382,24 @@ apply_metadata_changes (struct node *np, const inode_replay_state_t *state,
   if (changes > 0)
     {
 #if JOURNAL_REPLAY_DRY_RUN
-      JOURNAL_LOG_DEBUG
-	("[DRY_RUN] inode %u: path %s, %d metadata changes would be applied: [%s]",
-	 state->ino, path, changes, change_desc);
+      JOURNAL_LOG_DEBUG ("[DRY_RUN] inode %" PRIu32
+			 ": path %s, %d metadata changes would be applied: [%s]",
+			 state->ino, path, changes, change_desc);
 #else
       np->dn_stat_dirty = 1;
       np->dn_set_ctime = 1;
       diskfs_node_update (np, 0);
-      JOURNAL_LOG_DEBUG
-	("inode %u: path %s, %d metadata changes applied: [%s]", state->ino,
-	 path, changes, change_desc);
+      JOURNAL_LOG_DEBUG ("inode %" PRIu32
+			 ": path %s, %d metadata changes applied: [%s]",
+			 state->ino, path, changes, change_desc);
 #endif
     }
   else
     {
-      JOURNAL_LOG_DEBUG ("inode %u: path %s, no changes needed", state->ino,
-			 path);
+      JOURNAL_LOG_DEBUG ("inode %" PRIu32 ": path %s, no changes needed", state->ino, "");	//path);
     }
 
-  return changes;
-}
-
-error_t
-apply_node_replay (inode_replay_state_t *state, struct node *fs_root,
-		   struct node *restore_root, struct protid *cred)
-{
-  if (state->ino < JOURNAL_REPLAY_MIN_INO)
-    {
-      JOURNAL_LOG_DEBUG ("inode %u below REPLAY_MIN_INO (%d), skipping",
-			 state->ino, JOURNAL_REPLAY_MIN_INO);
-      return 0;
-    }
-
-  struct node *np = NULL;
-  char full_path[JOURNAL_NORMALIZED_PATH_MAX] = { 0 };
-  error_t err = diskfs_cached_lookup ((ino_t) state->ino, &np);
-
-  if (!err && np && node_matches_fingerprint (np, state))
-    {
-      if (should_skip_inode (state, np))
-	{
-	  diskfs_nput (np);
-	  return 0;
-	}
-    }
-  else
-    {
-      if (np)
-	diskfs_nput (np);
-      err = journal_resolve_full_path (state->resolved_path, state->name,
-				       full_path, sizeof (full_path));
-      if (err)
-	return err;
-      err =
-	find_by_path_or_create (full_path, state, fs_root, restore_root, cred,
-				&np);
-      if (!np)
-	return err;
-    }
-
-  apply_metadata_changes (np, state,
-			  full_path[0] ? full_path : state->resolved_path);
   diskfs_nput (np);
   return 0;
 }
+
